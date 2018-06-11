86bda6a3279075479c1e48fd4bca6edb0ab807d6
tyh汤雨欢 <yh.tang@ctrip.com>
2018-06-11 16:28:50 +0800

    add LocaleProvider

--- docs/src/pages/demos/pickers/BasicDatePicker.js
+++ docs/src/pages/demos/pickers/BasicDatePicker.js
@@ -1,22 +1,37 @@
-import React, { Fragment, PureComponent } from 'react';
+import moment from 'moment';
+import 'moment/locale/en-ca';
+import 'moment/locale/zh-cn'; // this is the important bit, you have to import the locale your'e trying to use.
+import MomentUtils from 'material-ui-pickers/utils/moment-utils';
+import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
+import React, { Fragment, Component, PureComponent } from 'react';
 import DatePicker, { withDateTimePicker } from 'react-material/Picker';
+import { Icon, IconButton, Menu, MenuItem } from '@material-ui/core';
+import Button from 'react-material/Button'
+import LocaleProvider from 'react-material/LocaleProvider'
 
-class BasicDatePicker extends PureComponent {
-  state = {
-    selectedDate: new Date(),
+
+class BasicDate extends PureComponent {
+  constructor(props){
+    super(props)
+    this.state={
+      selectedDate: new Date(),
+    }
+  }
+  handleDateChange = (data) =>{
+    this.setState({selectedDate: date});
   }
 
-  handleDateChange = (date) => {
-    this.setState({ selectedDate: date });
-  }
-
-  render() {
-    const { selectedDate } = this.state;
-
+  render(){
+    const { selectedDate } = this.state.selectedDate
+    const locale = localeMap[this.props.currentLocale];
+    moment.locale(locale);
+    
     return (
       <Fragment>
         <div className="picker">
           <DatePicker
+            okLabel={(locale==='zh-cn')?"确定":"OK"}
+            cancelLabel={(locale==='zh-cn')?"取消":"CANCEL"}
             label="Basic example"
             value={selectedDate}
             onChange={this.handleDateChange}
@@ -26,6 +41,9 @@ class BasicDatePicker extends PureComponent {
 
         <div className="picker">
           <DatePicker
+            okLabel={(locale==='zh-cn')?"确定":"OK"}
+            cancelLabel={(locale==='zh-cn')?"取消":"CANCEL"}
+            clearLabel={(locale==='zh-cn')?"清除":"CLEAR"}
             label="Clearable"
             clearable
             disableFuture
@@ -38,6 +56,9 @@ class BasicDatePicker extends PureComponent {
 
         <div className="picker">
           <DatePicker
+            okLabel={(locale==='zh-cn')?"确定":"OK"}
+            cancelLabel={(locale==='zh-cn')?"取消":"CANCEL"}
+            todayLabel={(locale==='zh-cn')?"今天":"TODAY"}
             label="With today button"
             showTodayButton
             maxDateMessage="Date must be less than today"
@@ -50,5 +71,56 @@ class BasicDatePicker extends PureComponent {
     );
   }
 }
+//export default withDateTimePicker()(BasicDatePicker)
+
+
+const ButtonGroup = Button.Group;
+const radio = true
+const localeMap = {
+  zhCn:'zh-cn',
+  enCa:'en-ca',
+};
+
+class BasicDatePicker extends Component{
+  constructor(props){
+    super(props);
+    this.state = { 
+      locale: 'zhCn',
+    };
+  }  
+
+  handleButtonClick =(type)=> (e)=>{
+    let locale=type
+    this.setState({
+      locale: locale,
+    })
+  }
+
+  render(){
+    let locale = localeMap[this.state.locale]
+    return(
+      <LocaleProvider locale={locale}> 
+        <MuiPickersUtilsProvider utils={MomentUtils} moment={moment} locale={locale}>
+          <BasicDate currentLocale={this.state.locale} />
+          <ButtonGroup>
+            <Button onClick={this.handleButtonClick('zhCn')} variant="raised" color="primary">中文</Button>
+            <Button onClick={this.handleButtonClick('enCa')} radio={radio} variant="raised" color="primary">EN</Button>
+          </ButtonGroup>
+        </MuiPickersUtilsProvider>
+      </LocaleProvider>
+    )
+  }
+}
 
 export default withDateTimePicker()(BasicDatePicker)
+
+
+
+
+
+
+
+
+
+
+

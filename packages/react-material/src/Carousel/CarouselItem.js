cf75823c9f91d8e704e7be101fe6feb475a16334
wgshao <wgshao@ctrip.com>
2018-06-22 18:40:03 +0800

    carousel fixxed

--- packages/react-material/src/Carousel/CarouselItem.js
+++ packages/react-material/src/Carousel/CarouselItem.js
@@ -5,7 +5,6 @@ import { withStyles } from '../styles';
 
 const styles = {
   imgwarp:{
-    maxWidth:'500px',
     float:'left',
     overflow:'hidden',
     backgroundSize: 'cover',
@@ -24,20 +23,21 @@ class CarouselItem extends React.Component {
     this.itemRef = React.createRef();
   }
   componentDidMount(){
-    const itemEl = ReactDOM.findDOMNode(this.itemRef.current),
-      parentEl = itemEl.parentNode.parentNode, 
-    h = parentEl.offsetHeight,
-    w = parentEl.offsetWidth;
-    itemEl.style.height= h+'px';
-    itemEl.style.width= w+'px';
-    itemEl.children[0].style.minHeight=h+'px';
-    itemEl.children[0].style.display='block';
+    // const itemEl = ReactDOM.findDOMNode(this.itemRef.current),
+    //   parentEl = itemEl.parentNode.parentNode, 
+    // h = parentEl.offsetHeight,
+    // w = parentEl.offsetWidth;
+    //itemEl.style.height= h+'px';
+    //itemEl.style.width= w+'px';
+    //itemEl.children[0].style.minHeight=h+'px';
+    //itemEl.children[0].style.display='block';
   }
 
   render() {
     const {data,index,classes,size} = this.props;
     return (
       <div ref={this.itemRef} style={{'width':size.width+'px','height':size.height+'px',backgroundImage:'url('+data.src+')'}} alt={data.alt} className={classes.imgwarp}>
+      <img src={data.src} alt={data.alt}/>
       </div>
     );
   }

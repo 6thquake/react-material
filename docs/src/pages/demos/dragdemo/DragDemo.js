9801ef272521b760bdf4667f50ff0344a55b7933
tsy唐思雨 <sy.tang@ctrip.com>
2018-06-22 09:21:39 +0800

    tsy/code

---
+++ docs/src/pages/demos/dragdemo/DragDemo.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { DragSource } from 'react-dnd';
+import { ItemTypes } from './Constants';
+
+/**
+ * Implements the drag source contract.
+ */
+const cardSource = {
+  beginDrag(props) {
+    return {
+      text: props.text
+    };
+  }
+};
+
+@DragSource(ItemTypes.CARD, cardSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
+}))
+export default class Card extends Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+
+    // Injected by React DnD:
+    connectDragSource: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired
+  };
+
+  render() {
+    const { isDragging, connectDragSource, text } = this.props;
+    return connectDragSource(
+      <div style={{ opacity: isDragging ? 0.5 : 1 }}>
+        {text}
+      </div>
+    );
+  }
+}

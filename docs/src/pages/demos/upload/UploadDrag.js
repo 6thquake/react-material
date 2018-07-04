c42a1d32a48cc1d47f1bc0d181ec2d6cb1b4f6e0
雨欢 <yuhuan@yuhuandeMacBook-Pro.local>
2018-07-04 17:02:05 +0800

    finish basic click to upload

---
+++ docs/src/pages/demos/upload/UploadDrag.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'react-material/styles';
+import {UploadDrag} from 'react-material/Upload';
+import Button from 'react-material/Button';
+class App extends React.Component {
+	uploadFunc=(data)=>{
+        console.log(data)
+	};
+    deleteFunc=(fileToDelete)=>{
+        console.log(fileToDelete)
+    }
+    render() {
+        return (
+                <UploadDrag acceptType={"*"} actionFunc={this.uploadFunc} disabled={false} multiple={true} deleteFile={this.deleteFunc}>
+
+                </UploadDrag>
+        );
+    }
+}
+export default App;
+
+
--- packages/react-material/src/Upload/index.js
+++ packages/react-material/src/Upload/index.js
@@ -2,3 +2,5 @@ export * from './Upload';
 export { default as Upload } from './Upload';
 export * from './UploadBasic';
 export { default as UploadBasic } from './UploadBasic';
+export * from './UploadDrag';
+export { default as UploadDrag } from './UploadDrag';

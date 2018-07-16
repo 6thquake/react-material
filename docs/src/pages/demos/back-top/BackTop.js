b041ebfb02ef3d29b8d031a13049ba8716b5509d
lyn李雅男 <liyn@dst62117-7.local>
2018-07-16 12:26:37 +0800

    backtop modify add container backtop button

--- docs/src/pages/demos/back-top/BackTop.js
+++ docs/src/pages/demos/back-top/BackTop.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'react-material/styles';
 import BackTop from 'react-material/BackTop';
+import Codearea from 'react-material/Codearea';
 
 const style = {
   height: '40px',
@@ -24,13 +25,66 @@ class App extends React.Component {
       <div>
         <BackTop onClick={this.doSomething.bind(this)} />
 
-        <p>默认按钮，向下滚动页面后，见右下角灰色按钮</p>
+        <p>默认按钮(整个网页)，向下滚动页面后，见右下角灰色按钮</p>
 
         <BackTop visibilityHeight={550} onClick={this.doSomething.bind(this)}>
           <div style={style}>UP</div>
         </BackTop>
 
-        <p> 自定义按钮，向下滚动页面后，见右下角蓝色按钮</p>
+        <p> 自定义按钮（整个网页），向下滚动页面后，见右下角蓝色按钮</p>
+
+        <h4>下面示例容器内容无关紧要，只是是为了撑开页面，出现元素内部滚动条</h4>
+
+        <BackTop container="#demoId" onClick={this.doSomething.bind(this)} />
+
+        <div id="demoId" style={{ height: '400px', overflowY: 'auto', marginTop: '20px' }}>
+          <p>使用字面值创建对象。</p>
+          <Codearea language="js">
+            {`
+               // bad
+                const item = new Object();
+
+               // good
+                const item = {};
+            `}
+          </Codearea>
+          <p>
+            如果你的代码在浏览器环境下执行，别使用 保留字 作为键值。这样的话在 IE8 不会运行。
+            更多信息。 但在 ES6 模块和服务器端中使用没有问题。
+          </p>
+          <Codearea language="js">
+            {`
+                // bad
+                 const superman = {
+                 default: { clark: 'kent' },
+                 private: true,
+                 };
+                // good
+                 const superman = {
+                 defaults: { clark: 'kent' },
+                 hidden: true,
+                 };
+            `}
+          </Codearea>
+
+          <p>使用同义词替换需要使用的保留字。</p>
+          <Codearea language="js">
+            {`
+               // bad
+                const superman = {
+                class: 'alien',
+                };
+               // bad
+               const superman = {
+                klass: 'alien',
+               };
+               // good
+                const superman = {
+                type: 'alien',
+                   };
+               `}
+          </Codearea>
+        </div>
       </div>
     );
   }
--- packages/react-material/src/BackTop/BackTop.js
+++ packages/react-material/src/BackTop/BackTop.js
@@ -5,15 +5,6 @@ import { Publish } from '@material-ui/icons';
 import PropTypes from 'prop-types';
 
 const styles = {
-  rmBackTop: {
-    zIndex: '10',
-    position: 'fixed',
-    right: '100px',
-    bottom: '50px',
-    height: '40px',
-    width: '40px',
-    cursor: 'pointer',
-  },
   rmBackTopContent: {
     paddingTop: '10px',
     transition: 'all .3s cubic-bezier(.645,.045,.355,1)',
@@ -24,23 +15,28 @@ const styles = {
     color: '#fff',
     textAlign: 'center',
   },
+
+  /* rmBackTop: {
+    zIndex: '10',
+    position: 'fixed',
+    right: '100px',
+    bottom: '50px',
+    height: '40px',
+    width: '40px',
+    cursor: 'pointer',
+  },*/
+  backTop: {
+    zIndex: '10000000',
+    position: 'fixed',
+    height: '40px!important',
+    width: '40px!important',
+    cursor: 'pointer',
+  },
+  windowBtn: {
+    right: '100px',
+    bottom: '50px',
+  },
 };
-
-function hasScrollbar() {
-  return document.body.scrollHeight > (window.innerHeight || document.documentElement.clientHeight);
-}
-
-function getScrollTop() {
-  var scrollPos;
-  if (window.pageYOffset) {
-    scrollPos = window.pageYOffset;
-  } else if (document.compatMode && document.compatMode != 'BackCompat') {
-    scrollPos = document.documentElement.scrollTop;
-  } else if (document.body) {
-    scrollPos = document.body.scrollTop;
-  }
-  return scrollPos;
-}
 
 class BackTop extends React.Component {
   static propTypes = {
@@ -53,10 +49,11 @@ class BackTop extends React.Component {
      * callback function when click BackTop button
      */
     onClick: PropTypes.func,
+
     /**
-     * custom BackTop button,must hava only child
+     * Id selector, element that needs to be listened the scroll event, the default value is window
      */
-    children: PropTypes.node,
+    container: PropTypes.string,
   };
 
   static defaultProps = {
@@ -72,6 +69,8 @@ class BackTop extends React.Component {
       customButton: props.children,
       onClick: props.onClick,
       showBackTop: false,
+      fixLeft: 0,
+      fixTop: 0,
     };
 
     try {
@@ -83,58 +82,179 @@ class BackTop extends React.Component {
     }
   }
 
+  hasScrollbar() {
+    if (this.container == window) {
+      return (
+        document.body.scrollHeight > (window.innerHeight || document.documentElement.clientHeight)
+      );
+    } else {
+      return (
+        this.container.scrollHeight > this.container.clientHeight ||
+        this.container.offsetHeight > this.container.clientHeight
+      );
+    }
+  }
+
+  getScrollTop() {
+    var scrollPos;
+    if (this.container == window) {
+      if (window.pageYOffset) {
+        scrollPos = window.pageYOffset;
+      } else if (document.compatMode && document.compatMode != 'BackCompat') {
+        scrollPos = document.documentElement.scrollTop;
+      } else if (document.body) {
+        scrollPos = document.body.scrollTop;
+      }
+    } else {
+      scrollPos = this.container.scrollTop;
+    }
+
+    return scrollPos;
+  }
+
   handleScroll(event) {
-    if (hasScrollbar()) {
-      const scrollTop = getScrollTop();
+    if (this.hasScrollbar()) {
+      const scrollTop = this.getScrollTop();
       if (scrollTop > this.state.visibilityHeight) {
         this.setState({ showBackTop: true });
       } else {
         this.setState({ showBackTop: false });
       }
     }
+    if (this.container != window) {
+      this.resetPosition();
+    }
   }
 
   componentDidMount() {
-    if (hasScrollbar()) {
-      const scrollTop = getScrollTop();
+    this.showWindowBtn = false;
+    let idSel = this.props.container;
+    const { customButton } = this.state;
+    this.container = document.querySelector(idSel) || window;
+    if (this.hasScrollbar()) {
+      const scrollTop = this.getScrollTop();
       if (scrollTop > this.state.visibilityHeight) {
         this.setState({ showBackTop: true });
       }
     }
-    window.addEventListener('scroll', this.scrollHandler);
+    if (this.container == window) {
+      this.showWindowBtn = true;
+    }
+    window.addEventListener('scroll', this.scrollHandler); // 不管是什么类型的button 都需要监听浏览器滚动
+
+    if (this.container != window) {
+      this.container.addEventListener('scroll', this.scrollHandler);
+    }
+
+    if (this.container != window) {
+      this.resetPosition();
+    }
+  }
+
+  resetPosition() {
+    const { customButton } = this.state;
+    let top = 0; // container 下边界距窗口最上面的距离
+    let left = 0; // container 右边界距窗口最左边的距离
+
+    let fixLeft = 0; // backTop button 相对位置 left值
+    let fixTop = 0; // backTop button 相对位置 top值
+
+    top = this.container.getBoundingClientRect().bottom;
+    left = this.container.getBoundingClientRect().right;
+
+    if (customButton) {
+      customWith = customButton.getBoundingClientRect().width; // 自定义按钮本身的宽度
+      customHeight = customButton.getBoundingClientRect().height; // 自定义按钮本身的高度
+      if (customWith > 40) {
+        customWith = 40;
+      }
+      if (customHeight > 40) {
+        customHeight = 40;
+      }
+      fixLeft = left - customWith - 100;
+      fixLTop = top - customHeight - 50;
+    } else {
+      fixLeft = left - 40 - 100;
+      fixTop = top - 40 - 50;
+    }
+    this.setState({ fixLeft: fixLeft, fixTop: fixTop });
+  }
+
+  componentWillUnmount() {
+    this.container.removeEventListener('scroll', this.ths, false);
   }
 
   returnTop = e => {
     e.preventDefault();
     e.nativeEvent.stopImmediatePropagation();
     e.stopPropagation();
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    this.container == window
+      ? (document.body.scrollTop = document.documentElement.scrollTop = 0)
+      : (this.container.scrollTop = 0);
     this.props.onClick(e);
   };
 
   render() {
     const { classes } = this.props;
-    const { showBackTop, customButton } = this.state;
+    const { showBackTop, customButton, fixLeft, fixTop } = this.state;
 
-    const backTopButton = customButton ? (
-      <div>
-        <div
-          style={{ bottom: '100px' }}
-          onClick={e => this.returnTop(e)}
-          className={classes.rmBackTop}
-        >
-          {customButton}
-        </div>
-      </div>
-    ) : (
-      <div>
-        <div onClick={e => this.returnTop(e)} className={classes.rmBackTop}>
-          <div className={classes.rmBackTopContent}>
-            <Publish />
-          </div>
+    const windowBtnNoCustom = (
+      <div onClick={e => this.returnTop(e)} className={classes.backTop + ' ' + classes.windowBtn}>
+        <div className={classes.rmBackTopContent}>
+          <Publish />
         </div>
       </div>
     );
+
+    const windowBtnCustom = (
+      <div
+        style={{ bottom: '100px' }}
+        onClick={e => this.returnTop(e)}
+        className={classes.backTop + ' ' + classes.windowBtn}
+      >
+        {customButton}
+      </div>
+    );
+
+    const containerBtnNoCustom = (
+      <div
+        style={{ left: `${fixLeft}px`, top: `${fixTop}px` }}
+        onClick={e => this.returnTop(e)}
+        className={classes.backTop}
+      >
+        <div className={classes.rmBackTopContent}>
+          <Publish />
+        </div>
+      </div>
+    );
+
+    const containerBtnCustom = (
+      <div
+        style={{ bottom: '100px', left: `${fixLeft}px`, top: `${fixTop}px` }}
+        onClick={e => this.returnTop(e)}
+        className={classes.backTop}
+      >
+        {customButton}
+      </div>
+    );
+
+    let backTopButton = windowBtnNoCustom;
+
+    if (this.showWindowBtn) {
+      if (customButton) {
+        backTopButton = windowBtnCustom;
+      } else {
+        backTopButton = windowBtnNoCustom;
+      }
+    } else {
+      if (customButton) {
+        backTopButton = containerBtnCustom;
+      } else {
+        backTopButton = containerBtnNoCustom;
+      }
+    }
+
+    //const backTopButton = this.showWindowBtn ? (customButton ? windowBtnCustom : windowBtnNoCustom) :( customButton? containerBtnCustom : containerBtnNoCustom);
 
     return showBackTop ? backTopButton : null;
   }
